6fed5cf6bbad179a4e92469fecba07280e9c6676
ArtWen <49200611+ArtWen@users.noreply.github.com>
2020-06-29 21:32:10 +0200

    Admin panel (#2)
    * dodano admin panel z opcją wyświetlenia, edycji, i dodawania nowych projektów
    
    * css cleanup

---
+++ src/Form/ProjectType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Project;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/*  wygenerowane komendą php bin/console make:form
+ *  dla klasy Project
+*/
+class ProjectType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('summary')
+            ->add('description')
+            ->add('date')
+            ->add('Save', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Project::class,
+        ]);
+    }
+}
