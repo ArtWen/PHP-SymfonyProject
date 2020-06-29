5db97f9de696fea4335f6854bc480cbfc39280f8
ArtWen <art.wen@o2.pl>
2020-06-29 18:09:40 +0200

    Dodane proste logowanie zgodnie z informacjami na moodle

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * wygenerowane poprzez php bin/console make:auth
+     *
+     * @Route("/login", name="app_login")
+     */
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        if ($this->getUser()) {
+            return $this->redirectToRoute('index');
+        }
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    /**
+     * @Route("/logout", name="app_logout")
+     */
+    public function logout()
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    /**
+     * funkcja z poradnika przesłanego na platfornie moodle
+     * @Route("new_user", name="new_user")
+     */
+    public function newUser(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $user=new User();
+        $user->setusername('user');
+        $user->setPassword($passwordEncoder->encodePassword(
+            $user, 'user'));
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($user);
+        $entityManager->flush();
+        return new Response("<html><head></head><body>dodano użytkownika</body></html>");
+    }
+
+
+}
